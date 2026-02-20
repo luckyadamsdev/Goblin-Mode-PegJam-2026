d54eee4dc972aa78293ac0904ecbb4e8f3aaeb07
Lucky Adams <proximadams@gmail.com>
2026-02-20 17:01:31 -0600

    moves forward at constant speed

--- player_character/goblin.gd
+++ player_character/goblin.gd
@@ -11,11 +11,10 @@ func _physics_process(_delta: float) -> void:
 	#poll status from controller
 	if controller != null:
 		pass
-	# apply physics
-	
-	
-	pass
 
+	# apply physics
+	velocity = Vector3(0.0, 0.0, 1.0)
+	move_and_slide()
 
 func set_start_pos(new_pos:Node3D) -> void:
 	global_position = new_pos.global_position
