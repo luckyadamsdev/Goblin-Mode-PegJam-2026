b8f89677a97dfa61d5993fcd67178f468367f99b
rhill <rrh@horriblepain.com>
2026-02-20 16:55:54 -0600

    set up some race control

--- game_management/game_manager.gd
+++ game_management/game_manager.gd
@@ -9,13 +9,16 @@ var current_map:Map
 
 @export var cameras:Array[CameraMovement]
 
-
+@export var timer_label:Label
 
 func _ready() -> void:
 	instance = self
 	_load_map("res://map/map01.tscn")
 
 func _load_map(map_name:String) -> void:
+	if current_map != null:
+		clean_up_old_map()
+	
 	current_map = load(map_name).instantiate() as Map
 	add_child(current_map)
 	
@@ -24,8 +27,18 @@ func _load_map(map_name:String) -> void:
 	goblins[1].set_start_pos(current_map.goblin_2_start)
 	cameras[0].set_target(goblins[0])
 	cameras[1].set_target(goblins[1])
+	start_timer()
 	
 	current_map.end_zone.body_entered.connect(_on_check_player_finished_race)
 
 func _on_check_player_finished_race(body: Node3D) -> void:
+	if body is Goblin:
+		print("is goblin! ", (body as Goblin).player_id)
+
+func start_timer() -> void:
 	pass
+	
+func clean_up_old_map() -> void:
+	current_map.end_zone.body_entered.disconnect(_on_check_player_finished_race)
+	remove_child(current_map)
+	current_map.queue_free()
