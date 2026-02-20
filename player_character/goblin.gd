2662d72486b3d6b9cac1ffcba58b18a0c2c8ab1a
Lucky Adams <proximadams@gmail.com>
2026-02-20 17:21:16 -0600

    move based on direction player is facing

--- player_character/goblin.gd
+++ player_character/goblin.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 class_name Goblin
 
 const ACCELERATION := 0.2
-const MIN_SPEED := 1.0
+const MIN_SPEED := 3.0
 
 @export var player_id:int = 1
 @export var controller:GoblinController
@@ -20,9 +20,11 @@ func _physics_process(delta: float) -> void:
 
 	# apply physics
 	current_speed += ACCELERATION * delta
-	velocity.y -= gravity * delta
-	velocity.z = current_speed
+	var new_velocity: Vector3 = basis.z * current_speed
+	new_velocity.y = velocity.y - gravity * delta
+	velocity = new_velocity
 	move_and_slide()
+	# self.look_at(self.global_position + Vector3(0.5,0.3,0.9))
 
 func set_start_pos(new_pos:Node3D) -> void:
 	global_position = new_pos.global_position
