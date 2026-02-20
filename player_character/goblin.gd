45bbc1a63ff89d035804116654ed7ad1c1cd9466
Lucky Adams <proximadams@gmail.com>
2026-02-20 17:31:58 -0600

    rotation starts to work

--- player_character/goblin.gd
+++ player_character/goblin.gd
@@ -12,6 +12,7 @@ var gravity = ProjectSettings.get_setting('physics/3d/default_gravity')
 
 func _ready() -> void:
 	velocity = Vector3(0.0, 0.0, MIN_SPEED)
+	controller.set_player_id(player_id)
 
 func _physics_process(delta: float) -> void:
 	#poll status from controller
@@ -24,7 +25,7 @@ func _physics_process(delta: float) -> void:
 	new_velocity.y = velocity.y - gravity * delta
 	velocity = new_velocity
 	move_and_slide()
-	# self.look_at(self.global_position + Vector3(0.5,0.3,0.9))
+	self.look_at(self.global_position + Vector3(controller.h_axis,0.0,-1.0))
 
 func set_start_pos(new_pos:Node3D) -> void:
 	visible = true
--- player_character/goblin_controller.gd
+++ player_character/goblin_controller.gd
@@ -14,7 +14,18 @@ var button2changed:bool = false
 func _ready() -> void:
 	pass
 
+func _physics_process(_delta: float) -> void:
+	update_inputs()
+
 func update_inputs() -> void:
 	v_axis = Input.get_axis(control_def.up, control_def.down)
 	h_axis = Input.get_axis(control_def.left, control_def.right)
-	pass
+
+func set_player_id(id: int):
+	# Only call this function once or else values will be something like this: "p1_p1_up"
+	control_def.up      = 'p' + str(id) + '_' + control_def.up
+	control_def.down    = 'p' + str(id) + '_' + control_def.down
+	control_def.left    = 'p' + str(id) + '_' + control_def.left
+	control_def.right   = 'p' + str(id) + '_' + control_def.right
+	control_def.button1 = 'p' + str(id) + '_' + control_def.button1
+	control_def.button2 = 'p' + str(id) + '_' + control_def.button2
