c7678eab28adf23d43f0f9b3ff04e1ed467b4c81
Lucky Adams <proximadams@gmail.com>
2026-02-21 00:28:38 -0600

    players can jump

--- player_character/goblin.gd
+++ player_character/goblin.gd
@@ -2,6 +2,8 @@ extends CharacterBody3D
 class_name Goblin
 
 const ACCELERATION := 0.2
+const COYOTE_TIME := 0.2
+const JUMP_VELOCITY := 5.0
 const MIN_SPEED := 3.0
 
 @export var player_id:int = 1
@@ -13,6 +15,8 @@ var goblin_paused:bool = true
 var current_speed := MIN_SPEED
 var follow_direction:Node3D = null
 var gravity = ProjectSettings.get_setting('physics/3d/default_gravity')
+var time_since_jumped_in_air := 10.0
+var time_since_on_floor := 10.0
 
 func _ready() -> void:
 	velocity = Vector3(0.0, 0.0, MIN_SPEED)
@@ -23,17 +27,35 @@ func _physics_process(delta: float) -> void:
 	if goblin_paused:
 		return
 	# movement logic
-	handle_accelerate(delta)
+	_handle_accelerate(delta)
+	_handle_jumps(delta)
 	move_and_slide()
-	handle_rotation_controls(delta)
+	_handle_rotation_controls(delta)
 
-func handle_accelerate(delta: float) -> void:
+func _handle_jumps(delta: float) -> void:
+	# lets player jump even if they pressed the button too early or too late
+	if is_on_floor():
+		time_since_on_floor = 0.0
+		if time_since_jumped_in_air < COYOTE_TIME:
+			velocity.y += JUMP_VELOCITY
+			time_since_jumped_in_air = COYOTE_TIME
+	else:
+		time_since_on_floor += delta
+	time_since_jumped_in_air += delta
+	if controller.button_one_just_pressed():
+		if time_since_on_floor < COYOTE_TIME:
+			velocity.y += JUMP_VELOCITY
+			time_since_on_floor = COYOTE_TIME
+		else:
+			time_since_jumped_in_air = 0.0
+
+func _handle_accelerate(delta: float) -> void:
 	current_speed += ACCELERATION * delta
 	var new_velocity: Vector3 = basis.z * current_speed
 	new_velocity.y = velocity.y - gravity * delta
 	velocity = new_velocity
 
-func handle_rotation_controls(delta: float) -> void:
+func _handle_rotation_controls(delta: float) -> void:
 	follow_pivot.rotation.y = -1.0 * controller.h_axis * delta
 	self.look_at(follow_direction.global_position)
 
