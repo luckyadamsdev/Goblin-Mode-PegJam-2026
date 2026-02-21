c7377e21879e28b50a68202781c7c642ce3c051e
Anarcho-Coder <volin.thiessen@gmail.com>
2026-02-21 12:21:44 -0600

    Cleaned up directory

---
+++ models/player_character/goblin_control_definition.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name GoblinControlDefinition
+
+var up:StringName = "up"
+var down:StringName = "down"
+var left:StringName = "left"
+var right:StringName = "right"
+
+var button1:StringName = "b1"
+var button2:StringName = "b2"
