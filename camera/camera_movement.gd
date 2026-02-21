033b619785eda3373f11c4e64ccddd33216bc365
rhill <rrh@horriblepain.com>
2026-02-21 14:58:18 -0600

    some possible things for camera movement

--- camera/camera_movement.gd
+++ camera/camera_movement.gd
@@ -8,6 +8,8 @@ class_name CameraMovement
 
 @export var vertical_offset := 1.0
 
+@export var vertical_look_offset := 1.0
+
 @export var goblin : Goblin
 
 @export var starting_offset:Vector3 = Vector3(0.0, 2.0, -4.0)
@@ -18,11 +20,8 @@ class_name CameraMovement
 ## position that the camera would be 
 var target_position: Vector3
 
-var last_target_position: Vector3
 
-var last_target_velocity: Vector3
 
-var max_accel:float = 3.0
 
 const REMEMBER_TIME:float = 1.0 / 15.0
 
@@ -34,8 +33,6 @@ var remember_velocity:Array[Vector3]
 
 func _ready():
 	target_position = global_position
-	last_target_position = target_position
-	last_target_velocity = Vector3.ZERO
 	goblin.landed.connect(_on_landed)
 	goblin.jumped.connect(_on_jumped)
 
@@ -51,9 +48,12 @@ func _physics_process(delta : float):
 		if remember_velocity.size() > MAX_MEMORY:
 			remember_velocity.pop_front()
 	
-	var global_look_at_position := goblin.global_position
-	global_look_at_position.y += vertical_offset
-	var delta_v := target_position - global_look_at_position
+	var velocity_change := Vector3.ZERO
+	# TODO use remember velocity to lag 
+	
+	var global_track_position := goblin.global_position
+	global_track_position.y += vertical_offset
+	var delta_v := target_position - global_track_position
 	delta_v.y = 3.0
 	var follow_distance_with_speed := follow_distance
 	
@@ -61,16 +61,15 @@ func _physics_process(delta : float):
 		delta_v = delta_v.normalized() * follow_distance_with_speed
 		delta_v.y = follow_height
 		
-		last_target_position = target_position
-		target_position = global_look_at_position + delta_v
+		target_position = global_track_position + delta_v
 		
 		global_position = target_position
 		
-		last_target_velocity = (target_position - last_target_position)
+	var look_position:Vector3 = global_track_position
+	var look_at_offset:Vector3 = Vector3.UP * vertical_look_offset
 	
-	var look_position:Vector3 = global_look_at_position
-	
-	look_at(look_position + Vector3.UP, Vector3.UP)
+	# TODO maybe use velocity_change to offset look_at_offset?
+	look_at(look_position + look_at_offset, Vector3.UP)
 
 func set_target(new_target:Node3D) -> void:
 	goblin = new_target
