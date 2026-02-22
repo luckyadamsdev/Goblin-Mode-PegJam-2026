00837e6605d449201eb58a80933dee91ede4b0a1
Anarcho-Coder <volin.thiessen@gmail.com>
2026-02-21 22:00:11 -0600

    goblin's make sounds when landing, jumping, and surfing.

--- sound/goblin_audio_player.gd
+++ sound/goblin_audio_player.gd
@@ -1,4 +1,9 @@
 extends AudioStreamPlayer
+@export var landed_sound : AudioStream
+@export var jump_sound : AudioStream
+@export var hurt_sound : AudioStream
+@export var surf_sound : AudioStream
+var ambient_sound = surf_sound
 
 
 # Called when the node enters the scene tree for the first time.
@@ -10,10 +15,22 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func play_sound(sound: AudioStream) -> void:
+	if(playing):
+		stop()
+	stream = sound
+	if(is_instance_valid(stream)):
+		play()
+
+	
 
 func _on_goblin_jumped() -> void:
-	pass # Replace with function body.
-
+	play_sound(jump_sound)
+	ambient_sound = null
 
 func _on_goblin_landed() -> void:
-	pass # Replace with function body.
+	play_sound(jump_sound)
+	ambient_sound = surf_sound
+
+func _on_finished() -> void:
+	play_sound(ambient_sound)
