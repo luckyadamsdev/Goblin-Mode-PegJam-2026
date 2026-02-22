6a30946d16af0e68b225f7d3b4b373f6247ef605
Anarcho-Coder <volin.thiessen@gmail.com>
2026-02-21 19:00:00 -0600

    added audio stream player to goblin, and started a script for handling goblin noises

---
+++ sound/goblin_audio_player.gd
@@ -0,0 +1,19 @@
+extends AudioStreamPlayer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_goblin_jumped() -> void:
+	pass # Replace with function body.
+
+
+func _on_goblin_landed() -> void:
+	pass # Replace with function body.
