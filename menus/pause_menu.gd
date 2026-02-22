bce301d56a1099d27c1c6cb88db1dceb453a0187
Lucky Adams <proximadams@gmail.com>
2026-02-22 08:57:23 -0600

    minor tweaks to menus

--- menus/pause_menu.gd
+++ menus/pause_menu.gd
@@ -4,8 +4,8 @@ class_name PauseMenu
 func set_focus() -> void:
 	$Panel/ContinuePlaying.grab_focus()
 
-func _on_continue_playing_button_down() -> void:
+func _on_continue_playing_pressed() -> void:
 	GameManager.instance.unpause()
 
-func _on_back_button_down() -> void:
+func _on_main_menu_pressed() -> void:
 	GameManager.instance.back_to_main_menu()
